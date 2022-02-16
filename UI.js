7eec9a4cab4995f9c3c5d62552c0410086bbe211
居桉 <secan@hust.edu.cn>
2022-02-17 01:53:21 +0800

    Update UI.js

--- UI.js
+++ UI.js
@@ -1,69 +1,134 @@
 "ui";
 
-ui.layout(
-    <vertical>
-        <appbar>
-                <toolbar id="toolbar" title="强国助手" />
-        </appbar>
-        <text textSize="18sp" gravity="center" textColor="black" text="百度智能云 文字识别 API Key" />
-        <input id="AK" gravity="center" text="" />
-        <text textSize="18sp" gravity="center" textColor="black" text="百度智能云 文字识别 Secret Key" />
-        <input id="SK" gravity="center" text="" />
-        <button id="baidu" h="50" text="保存AK、SK，并检查正确性" />
-        <button id="study" h="50" text="开始学习" />
-        <button id="stop" h="50" text="停止运行" />
-        <button w="250" layout_gravity="center" id="about" text="使用说明" />
-    </vertical>
-);
-
 importClass(java.net.HttpURLConnection);
 importClass(java.net.URL);
 importClass(java.io.File);
 importClass(java.io.FileOutputStream);
+importClass(android.graphics.Color);
+
+ui.layout(
+    <vertical>
+        <appbar>
+                <toolbar id="toolbar" title="{{unescape('%u5F3A%u56FD%u52A9%u624B')}}" />
+        </appbar>
+        <horizontal>
+            <button id="autoservice" margin="5 15" padding="15" layout_weight="1" textSize="18sp" textColor="red" text="①点击授权" />
+            <button id="floaty" margin="5 15" padding="15" layout_weight="1" textSize="18sp" textColor="red" text="②点击授权" />
+            <button id="scap" margin="5 15" padding="15" layout_weight="1" textSize="18sp" textColor="red" text="③点击授权" />
+        </horizontal>
+        <text textSize="20sp" margin="20" gravity="center" textColor="black" text="四人赛、双人对抗 模式选择" />
+        <radiogroup margin="5 0 5 0">
+            <horizontal>
+                <radio id="ocr_offline" gravity="center" textSize="15sp" />
+                <radio id="ocr_online" gravity="center" textSize="15sp" />
+            </horizontal>
+        </radiogroup>
+        <text id="apikey" margin="0 30 0 0" textSize="20sp" gravity="center" textColor="black" text="文字识别 API Key" />
+        <input id="AK" margin="0 10 0 0" gravity="center" text="" />
+        <text id="secretkey" margin="0 20 0 0" textSize="20sp" gravity="center" textColor="black" text="文字识别 Secret Key" />
+        <input id="SK" margin="0 10 0 0" gravity="center" text="" />
+        <horizontal>
+            <button id="register" margin="20 20 10 0" layout_weight="1" h="50" text="点击注册百度智能云" />
+            <button id="baidu" margin="10 20 20 0" layout_weight="1" h="50" text="检查AK、SK" />
+        </horizontal>
+        <horizontal>
+            <text margin="20 30 0 0" textSize="16sp" textColor="black" text="默认延迟:" />
+            <spinner id="defaultdelay" margin="0 30 0 0" entries="1秒|2秒|3秒"/>
+            <text margin="0 30 0 0" textSize="16sp" textColor="black" text="OCR延迟:" />
+            <spinner id="ocrdelay" margin="0 30 20 0" entries="0毫秒|100毫秒|200毫秒|300毫秒|400毫秒|500毫秒"/>
+        </horizontal>
+        <horizontal>
+            <button id="stop" margin="20 30 10 0" layout_weight="1" h="120" bg="#FDE6E0" textSize="30sp" />
+            <button id="study" margin="10 30 20 0" layout_weight="1" h="120" bg="#C7EDCC" textSize="30sp" />
+        </horizontal>
+        <button w="250" h="60" margin="0 20 0 0" layout_gravity="center" id="update" textSize="18sp" />
+    </vertical>
+);
+
+ui.study.setText("开 始\n学 习");
+ui.stop.setText("结 束\n运 行");
+ui.ocr_offline.setText("PaddleOCR\n无需注册、本地识别\n准确率较低、速度中等\n视CPU而异");
+ui.ocr_online.setText("百度智能云\n注册简单、在线识别\n准确率较高、速度较快\n免费额度、个人够用");
+online_ocr_visibility(8);
+ui.update.visibility = 8;
 
 http.__okhttp__.setTimeout(10000);
 
+check_autoservice();
+check_floaty();
+check_scap();
+
+var BAIDUAPI = storages.create("BAIDUAPI");
+var CONFIG = storages.create("CONFIG");
+ui.AK.setText(BAIDUAPI.get("AK", ""));
+ui.SK.setText(BAIDUAPI.get("SK", ""));
+var execution = "";
+
+if (CONFIG.get("OCR", 0) && BAIDUAPI.get("AK", "") && BAIDUAPI.get("SK", "")) {
+    ui.ocr_online.checked = true;
+    online_ocr_visibility(0);
+} else {
+    ui.ocr_offline.checked = true;
+    CONFIG.put("OCR", 0);
+}
+
+ui.defaultdelay.setSelection(CONFIG.get("DEFAULT_DELAY", 0));
+ui.ocrdelay.setSelection(CONFIG.get("OCR_DELAY", 0));
+
 // 版本更新检查
-var apkurl = "https://gitee.com/sec-an/js/attach_files/967567/download/110.apk";
-if (app.versionName != "1.1.0") {
+var apkurl = "https://gitee.com/sec-an/js/attach_files/968866/download/v1.1.1.apk";
+var latest_version = "1.1.1";
+if (CONFIG.get("NO_UPDATE", 0) && (app.versionName != latest_version)) {
+    ui.update.visibility = 0;
+    ui.update.setText("点击更新至最新版v" + latest_version);
+} else if (app.versionName != latest_version) {
     checkversion();
 }
 
-var storage = storages.create("BAIDUAPI");
-// storage.clear();
-console.log(storage.get("AK", ""));
-console.log(storage.get("SK", ""));
-ui.AK.setText(storage.get("AK", ""));
-ui.SK.setText(storage.get("SK", ""));
-
 var thread = null;
 
-ui.baidu.click(function () {
-    threads.shutDownAll();
-    thread = threads.start(function () {
-        let AK = String(ui.AK.getText());
-        let SK = String(ui.SK.getText());
-        console.log(AK);
-        console.log(SK);
-        if (AK) storage.put("AK", AK);
-        if (SK) storage.put("SK", SK);
-        var res = http.post(
-            'https://aip.baidubce.com/oauth/2.0/token', {
-                grant_type: 'client_credentials',
-                client_id: storage.get("AK", ""),
-                client_secret: storage.get("SK", "")
-            }
-        ).body.json();
-        if ("error" in res) {
-            toast("AK、SK存在错误");
-            console.log("AK、SK存在错误");
-            console.log(storage.get("AK", ""));
-            console.log(storage.get("SK", ""));
-            return;
+ui.autoservice.click(function () {
+    check_autoservice();
+});
+
+ui.floaty.click(function () {
+    check_floaty();
+});
+
+ui.scap.click(function () {
+    threads.start(function () {
+        if (!requestScreenCapture()) {
+            toast("请求截图失败");
+        } else {
+            ui.run(() => {
+                ui.scap.visibility = 8;
+            });
         }
-        toast("AK、SK正确");
-        console.log("AK、SK正确");
     });
+});
+
+ui.ocr_offline.on("check", (checked) => {
+    if (checked) {
+        ui.ocr_online.checked = false;
+        CONFIG.put("OCR", 0);
+        online_ocr_visibility(8);
+    }
+});
+
+ui.ocr_online.on("check", (checked) => {
+    if (checked) {
+        ui.ocr_offline.checked = false;
+        CONFIG.put("OCR", 1);
+        online_ocr_visibility(0);
+    }
+});
+
+ui.register.click(function () {
+    app.openUrl("https://cloud.baidu.com/doc/OCR/s/dk3iqnq51");
+});
+
+ui.baidu.click(function () {
+    check_baidu_api();
 });
 
 ui.study.click(function () {
@@ -72,66 +137,151 @@ ui.study.click(function () {
         alert("注意", "脚本正在运行，请结束之前进程");
         return;
     }
-    threads.start(function () {
-        let url = [
-            'https://gitee.com/sec-an/js/raw/master/helper.js',
-            'http://cdn.sec-an.cn/Better-Auto-XXQG/helper.js',
-            'https://github.secan.workers.dev/https://raw.githubusercontent.com/sec-an/Better-Auto-XXQG/main/helper.js',
-            'https://cdn.jsdelivr.net/gh/sec-an/Better-Auto-XXQG@main/helper.js',
-            'https://raw.githubusercontent.com/sec-an/Better-Auto-XXQG/main/helper.js'
-        ];
-        for (var i = 0; i < url.length; i++) {
-            try {
-                let res = http.get(url[i]);
-                console.log(i + ":" + res.statusCode);
-                if (res.statusCode == 200) {
-                    var helper = res.body.string();
-                    break;
-                } else {
-                    toastLog()('助手脚本:地址' + i + '下载失败');
-                }
-            } catch (error) {}
-        }
-        toastLog("开始积分判断运行");
-        engines.execScript("强国助手", helper);
-    });
-});
-
-ui.stop.click(function () {
-    if (thread != null && thread.isAlive()) {
-        threads.shutDownAll();
-        toast("停止运行！");
-        console.hide();
+    if (CONFIG.get("OCR", 0) && !BAIDUAPI.get("AK", "")) {
+        check_baidu_api();
+        toastLog("请先检查AK、SK\n之后再次点击开始学习");
+        return;
+    }
+    CONFIG.put("DEFAULT_DELAY", ui.defaultdelay.getSelectedItemPosition());
+    CONFIG.put("OCR_DELAY", ui.ocrdelay.getSelectedItemPosition());
+    if (!CONFIG.get("OCR", 0) || BAIDUAPI.get("AK", "")) {
+        threads.start(function () {
+            let url = [
+                'https://gitee.com/sec-an/js/raw/master/helper.js',
+                'http://cdn.sec-an.cn/Better-Auto-XXQG/helper.js',
+                'https://github.secan.workers.dev/https://raw.githubusercontent.com/sec-an/Better-Auto-XXQG/main/helper.js',
+                'https://cdn.jsdelivr.net/gh/sec-an/Better-Auto-XXQG@main/helper.js',
+                'https://raw.githubusercontent.com/sec-an/Better-Auto-XXQG/main/helper.js'
+            ];
+            for (var i = 0; i < url.length; i++) {
+                try {
+                    let res = http.get(url[i]);
+                    console.log(i + ":" + res.statusCode);
+                    if (res.statusCode == 200) {
+                        var helper = res.body.string();
+                        break;
+                    } else {
+                        toastLog()('助手脚本:地址' + i + '下载失败');
+                    }
+                } catch (error) {}
+            }
+            toastLog("开始积分判断运行");
+            execution = engines.execScript("强国助手", helper);
+        });
     } else {
-        toast("没有线程在运行！");
+        toastLog("请使用PaddleOCR\n或检查百度AK、SK");
     }
 });
 
-ui.about.click(function () {
-    alert("使用说明",
-        "强国助手"
-    )
+ui.stop.click(function () {
+    execution.getEngine().forceStop();
 });
 
-function checkversion() {
-    var releaseNotes = "版本 v1.1.0\n" +
-        "更新日志:\n" +
-        "* 使用AutoX重新打包\n" +
-        "* 增加对PaddleOCR的支持\n";
-    dialogs.build({
-        title: "发现新版本",
-        content: releaseNotes,
-        positive: "立即下载",
-        negative: "取消",
-        neutral: "到浏览器下载",
-        cancelable: false
-    })
-    .on("positive", download)
-    .on("neutral", () => {
-        app.openUrl(apkurl);
-    })
-    .show();
+ui.update.click(function () {
+    if (app.versionName != latest_version) {
+        CONFIG.put("NO_UPDATE", 0);
+        checkversion();
+    } else {
+        toast("当前已经是最新版本！");
+    }
+});
+
+function online_ocr_visibility(i) {
+    ui.apikey.visibility = i;
+    ui.AK.visibility = i;
+    ui.secretkey.visibility = i;
+    ui.SK.visibility = i;
+    ui.register.visibility = i;
+    ui.baidu.visibility = i;
 }
+
+function check_autoservice() {
+    if (auto.service == null) {
+        toast("请开启脚本的无障碍服务");
+        auto.waitFor();
+    } else {
+        ui.autoservice.visibility = 8;
+    }
+}
+
+function check_floaty() {
+    if (!floaty.checkPermission()) {
+        toast("本脚本需要悬浮窗权限来显示悬浮窗，请在随后的界面中允许并重新运行本脚本。");
+        floaty.requestPermission();
+    } else {
+        ui.floaty.visibility = 8;
+    }
+}
+
+function check_scap() {
+    threads.shutDownAll();
+    threads.start(function () {
+        threads.start(function () {
+            var beginBtn;
+            sleep(500);
+            if (beginBtn = classNameContains("Button").textContains("开始").findOne(1000));
+            else(beginBtn = classNameContains("Button").textContains("允许").findOne(1000));
+            if (beginBtn) beginBtn.click();
+        });
+        if (!requestScreenCapture()) {
+            toast("请求截图失败");
+        } else {
+            ui.run(() => {
+                ui.scap.visibility = 8;
+            });
+        }
+    });
+}
+
+function check_baidu_api() {
+    thread = threads.start(function () {
+        let AK = String(ui.AK.getText());
+        let SK = String(ui.SK.getText());
+        var res = http.post(
+            'https://aip.baidubce.com/oauth/2.0/token', {
+                grant_type: 'client_credentials',
+                client_id: AK,
+                client_secret: SK
+            }
+        ).body.json();
+        if ("error" in res) {
+            toastLog("API Key或Secret Key存在错误");
+            console.log(AK);
+            console.log(SK);
+            ui.AK.setText(BAIDUAPI.get("AK", ""));
+            ui.SK.setText(BAIDUAPI.get("SK", ""));
+            BAIDUAPI.put("AK", "");
+            BAIDUAPI.put("SK", "");
+        } else {
+            toastLog("API Key、Secret Key正确，且已缓存");
+            BAIDUAPI.put("AK", AK);
+            BAIDUAPI.put("SK", SK);
+        }
+    });
+}
+
+function checkversion() {
+    var releaseNotes = "版本 v" + latest_version + "\n" +
+        "更新日志:\n" +
+        "* AutoX回滚到 rhino-1.7.13，重新打包\n";
+    dialogs.build({
+            title: "发现新版本",
+            content: releaseNotes,
+            positive: "立即下载",
+            negative: "取消",
+            neutral: "浏览器下载",
+            checkBoxPrompt: "不再提示",
+            cancelable: false
+        })
+        .on("positive", download)
+        .on("neutral", () => {
+            app.openUrl(apkurl);
+        })
+        .on("check", (checked) => {
+            CONFIG.put("NO_UPDATE", 1);
+        }).show();
+}
+
 //打开下载进度面板
 function download() {
     downloadDialog = dialogs.build({
@@ -143,9 +293,9 @@ function download() {
         autoDismiss: false,
         cancelable: false
     }).show();
-    //执行下载
     startDownload();
 }
+
 //下载apk的主方法体
 function startDownload() {
     threads.start(function () {
