4effa36fbc302c6a0e0742f0d6e219d868ab6c34
sec-an <sec_an@foxmail.com>
2022-08-02 00:59:37 +0000

    update 0.js.

--- 0.js
+++ 0.js
@@ -145,7 +145,7 @@ device.keepScreenOn(3600 * 1000);
 fInfo("检测题库更新");
 // var git_head = "https://gh.api.99988866.xyz/";
 // const update_info = get_tiku_by_http(git_head+"https://raw.githubusercontent.com/songgedodo/tiku/main/info.json");
-const update_info = get_tiku_by_http("https://mart-17684809426.coding.net/p/tiku/d/tiku/git/raw/master/info.json");
+const update_info = get_tiku_by_http("https://gitcode.net/m0_64980826/songge_tiku/-/raw/master/info.json");
 fInfo("正在加载对战题库......请稍等\n题库版本:"+update_info["tiku_version"]);
 var tiku = [];
 try {tiku = get_tiku_by_http(update_info["tiku_link"]);}
