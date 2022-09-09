a7c43f3bb282a24ae9f0c08c1e426d6a46ee3584
sec-an <sec_an@foxmail.com>
2022-09-09 19:42:09 +0800

    Update 0.js

--- 0.js
+++ 0.js
@@ -2550,7 +2550,7 @@ if (zhanghao) {
     let userinfo = zh.split(/:|：/);
     zhanghao_list.push(userinfo);
   };
-  if (zhanghao_list.length > 3) {zhanghao_list.length = 3;}
+  // if (zhanghao_list.length > 3) {zhanghao_list.length = 3;}
   //console.verbose(zhanghao_list);
   for (let userinfo of zhanghao_list) {
     console.verbose(userinfo);
