ddfdb0b9aa8d3eb1d01f972de496ba19250a2e48
sec-an <sec_an@foxmail.com>
2022-09-09 21:42:05 +0800

    V2.33.0下载

--- UI.js
+++ UI.js
@@ -520,7 +520,7 @@ ui.emitter.on("options_item_selected", (e, item)=>{
             app.openUrl("https://github.com/sec-an/Better-Auto-XXQG");
             break;
         case "V2.33.0下载":
-            download("https://android-apps.pp.cn/fs08/2021/12/28/3/110_f37c420b0944cb7b9f60a2ad9b5518d2.apk?yingid=web_space&packageid=500730793&md5=664bb7bdcae57be189fc86100f4371c4&minSDK=21&size=191654161&shortMd5=1fee0bd160d08108a9d9e5f4773ce741&crc32=3879122865&did=ad484a175e19d0928044435e24bf03cb");
+            app.openUrl("https://android-apps.pp.cn/fs08/2021/12/28/3/110_f37c420b0944cb7b9f60a2ad9b5518d2.apk?yingid=web_space&packageid=500730793&md5=664bb7bdcae57be189fc86100f4371c4&minSDK=21&size=191654161&shortMd5=1fee0bd160d08108a9d9e5f4773ce741&crc32=3879122865&did=ad484a175e19d0928044435e24bf03cb");
             break;
     }
     e.consumed = true;
