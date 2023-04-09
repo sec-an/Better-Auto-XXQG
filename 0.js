3c06f5588d3bc1c2f59b2495a1d5be154fbb36dc
sec-an <sec_an@foxmail.com>
2023-04-09 16:29:44 +0800

    滑动验证改震动2秒提醒

--- 0.js
+++ 0.js
@@ -2398,61 +2398,9 @@ function winReshow() {
 }
 
 function noverify() {
-  let noverify_thread = threads.start(function () {
-    //在新线程执行的代码
-    while (true) {
-      textContains("请按照说明拖动滑块").waitFor();
-      fInfo("检测到滑动验证");
-      if (!Number(slide_verify)) {
-        fInfo("未开启自动验证");
-        break
-      } else {
-        var delay = Number(slide_verify);
-      }
-      text("请按照说明拖动滑块").waitFor();
-      let bound = textContains("请按照说明拖动滑块").findOne().parent().child(1).bounds();
-      let hua_bound = text("请按照说明拖动滑块").findOne().bounds();
-      let x_start = bound.centerX();
-      let dx = x_start - hua_bound.left;
-      let x_end = (hua_bound.right - dx) * random(5.1, 6.0) / 10; // “hua_bound.right - dx”表示拖动到最后，为了准确率更高点 拖动到一半左右即可
-      let x_mid = (x_end - x_start) * random(5, 7) / 10 + x_start;
-      let back_x = (x_end - x_start) * random(2, 3) / 10;
-      let y_start = random(bound.top, bound.bottom);
-      let y_end = random(bound.top, bound.bottom);
-      // log("y_start:", y_start, "x_start:", x_start, "x_mid:", x_mid, "x_end:", x_end);
-      x_start = random(x_start - 7, x_start);
-      x_end = random(x_end, x_end + 10);
-      gesture(random(delay, delay + 200), [x_start, y_start], [x_end, y_end]);
-      //swipe(x_start, y_start, x_end, y_end, random(900,1000));
-      sleep(random(1000, 1500));
-      while (textContains("滑动位置不对哦，请再试一次").exists()) {
-        text("请按照说明拖动滑块").waitFor();
-        text("icon/24/icon_Y_shuaxin").findOne().parent().click();
-        sleep(random(1000, 1500));
-        continue;
-      }
-      if (textContains("刷新").exists()) {
-        click("刷新");
-        sleep(random(1000, 1500));
-        continue;
-      }
-      if (textContains("网络开小差").exists()) {
-        click("确定");
-        sleep(random(1000, 1500));
-        continue;
-      }
-      if (text("当前功能使用人数过多，请稍后重试").exists()) {
-        click("确定");
-        id("btn_next").findOne().click();
-        sleep(random(1000, 1500));
-        continue;
-      }
-      fInfo("已完成滑动验证，若滑动失败请在Pro版配置中调整滑动时间");
-      sleep(1000);
-      fClear();
-    }
-  });
-  return noverify_thread;
+  return threads.start(function () {
+    for (;;) fClear(), textContains("访问异常").waitFor(), fInfo("检测到滑动验证，请尽快滑动"), device.vibrate(2000), textContains("刷新").exists() ? click("刷新") : textContains("网络开小差").exists() ? click("确定") : sleep(1000)
+  })
 }
 
 function displayProp(obj) {
